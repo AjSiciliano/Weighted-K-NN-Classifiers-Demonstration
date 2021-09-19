b99229734b25faf76ece2f87e831d964318d90a0
Chris Wu <christopherjwu2000@gmail.com>
2021-09-19 14:17:51 -0400

    push breastcancer file

--- breastcancer.py
+++ breastcancer.py
@@ -1,31 +1,58 @@
 import csv
 import math
 from collections import Counter
+import numpy as np
 import pandas as pd
+import random
+
+filename = "datasets/breast-cancer-wisconsin.data"
 
 # initializing the titles and rows list
+fields = []
+total_set = [] #first half training, second half testing
+normalized_set = []
 benign_or_malignant = []
 
+def correlate_freqs(v1,v2):
+
+    num_correct = 0
+
+    for i in range(len(v1)):
+        
+        if (v1[i] == v2[i]):
+            num_correct += 1
+
+    return num_correct
+
+
 def normalization(array):
-    maximum = max(array)
-    minimum = min(array)
+
     normalized_list = []
-    for element in array:
-        e = element
-        normalized_list.append((e - minimum) / (maximum - minimum))
+
+    maximum = np.maximum.reduce(full_data)
+    minimum = np.minimum.reduce(full_data)
+
+    for i in range(len(array)):
+        e = array[i]
+
+        normalized_list.append((e - minimum[i]) / (maximum[i] - minimum[i]))
 
     return normalized_list
 
-df = pd.read_csv('/Users/chriswu/Downloads/breast-cancer-wisconsin.data')
+
+df = pd.read_csv('datasets/breast-cancer-wisconsin.data')
 df.replace('?',-99999,inplace=True)
 #drop unnecessary columns (id and class) ...also test without drop
 df.drop(['id'],1,inplace=True)
+df.drop(['class'],1,inplace=True)
 
 #df has random quotes. Convert all to float
 full_data = df.astype(float).values.tolist()
-
-for row in full_data:
-    benign_or_malignant.append(row.pop())
+        
+for row in range(len(full_data)):
+    #print(total_set[row])
+    normalized_set.append(normalization(full_data[row]))
+    # print(normalized_set[row])
 
 def euclidian_distance(row1, row2):
     distance = 0
@@ -34,19 +61,125 @@ def euclidian_distance(row1, row2):
         distance += math.pow(float(row1[features]) - float(row2[features]), 2)
 
     return math.pow(distance, 1/2)
-    
-def nearest_neighbors(element, k):
-    distances = []
+
+mid = len(normalized_set)//2
+
+def unweighted_KNN_classification(test_set, element,k):
+    total_distances = []
     index = 0
-    for x in full_data:
-        distances.append([euclidian_distance(x,element),  benign_or_malignant[index]])
-        index+=1
-    distances.sort()
-    #excludes element in search and adds new nearest neighbor
-    print([distances[i] for i in range(k+1)[1:k+1]])
+    
+    for x in test_set:
+        total_distances.append([euclidian_distance(x,element), benign_or_malignant[index]])
+        index += 1
 
-    return [distances[i][1] for i in range(k)]
+    total_distances.sort()
 
-NN = nearest_neighbors(full_data[8], 3)
+    distances_to_k = [total_distances[i] for i in range(k)]
 
-print(Counter(NN).most_common(1))
+    #we do the implementation below because for any k value, we need to keep track of the classes that appear to be our NN
+    above_dict = {2:[],4:[]}
+
+    for distance in distances_to_k:
+        #if distance[-1] == (float)above_dict[i]:
+        above_dict[int(distance[-1])].append(distance[0])
+
+    new_list = {0,0}
+
+    for i in above_dict: 
+
+        new_list[(i)] = len(above_dict[i])
+
+
+    return max(new_list, key=new_list.get)
+
+list_of_ks = [77]
+
+set_of_predictions = []
+
+for k in list_of_ks:
+
+    predictions = []
+
+    for x in range(mid):
+
+        predictions.append(unweighted_KNN_classification(normalized_set[mid:], normalized_set[x], k))
+
+    set_of_predictions.append(predictions)
+
+total = 0
+
+for x in set_of_predictions:
+    
+    total += correlate_freqs(x, benign_or_malignant[:mid])
+
+total = total / len(set_of_predictions)
+
+print( "Average error rate w/o weights: " + str(1 - (total / len(benign_or_malignant[:mid]))) )
+print( "Average error rate w/o weights: " + str(total)) 
+
+def weighted_KNN_classification(test_set, element,k):
+    total_distances = []
+    
+    index = 0
+    
+    for x in test_set:
+        print(x)
+        total_distances.append([euclidian_distance(x,element),  benign_or_malignant[mid + index]])
+        index += 1
+
+    total_distances.sort()
+
+    distances_to_k = [total_distances[i] for i in range(k)]
+    distances = [total_distances[i][0] for i in range(len(distances_to_k))]
+
+
+    maximum = max(distances)
+    minimum = min(distances)
+
+    above_dict = {2:[],4:[]}
+
+    for distance in distances_to_k:
+        #if distance[-1] == (float)above_dict[i]:
+        above_dict[int(distance[-1])].append(distance[0])
+
+    new_list = [0,0]
+
+    for i in above_dict: 
+       
+        for d in above_dict[i]:
+
+            if maximum != minimum:
+                new_list[i] += ((maximum - d) / (maximum - minimum))
+            else:
+                new_list[i] += 1
+
+
+    return new_list.index(max(new_list))
+    
+
+
+
+mid = len(normalized_set)//2
+
+set_of_predictions = []
+
+for k in list_of_ks:
+
+    predictions = []
+
+    for x in range(mid):
+
+        predictions.append(weighted_KNN_classification(normalized_set[mid:], normalized_set[x], k))
+
+    set_of_predictions.append(predictions)
+
+total = 0
+
+for x in set_of_predictions:
+    # print(x)
+    total += correlate_freqs(x, benign_or_malignant[:mid])
+
+total = total / len(set_of_predictions)
+
+print( "Average error rate w/weights: " + str(1 - (total / len(benign_or_malignant[:mid]))) )
+print( "Average error rate w/weights: " + str(total) )
