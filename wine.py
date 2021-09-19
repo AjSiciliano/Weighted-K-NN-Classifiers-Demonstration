389529ea037254e6259491bbca5b3b759b9d97ba
streetbeat <streetbeatdev@gmail.com>
2021-09-18 20:39:03 -0400

    new

--- wine.py
+++ wine.py
@@ -75,34 +75,35 @@ def euclidian_distance(row1, row2):
 
     return math.pow(distance, 1/2)
     
-def nearest_neighbors(element, k):
-    distances = []
-    index = 0
-    for x in test_set:
+# def nearest_neighbors(element, k):
+#     distances = []
+#     index = 0
+#     for x in test_set:
     
-        distances.append([euclidian_distance([x[i] for i in range(len(x)-1)],element), index,  qualities[index]])
-        index += 1
+#         distances.append([euclidian_distance([x[i] for i in range(len(x)-1)],element), index,  qualities[index]])
+#         index += 1
 
-    distances.sort()
-    #excludes element in search and adds new nearest neighbor
-    print([distances[i] for i in range(k+1)[1:k+1]])
+#     distances.sort()
+#     #excludes element in search and adds new nearest neighbor
+#     print([distances[i] for i in range(k+1)[1:k+1]])
 
-    return [distances[i][2] for i in range(k)]
+#     return [distances[i][2] for i in range(k)]
 
 # NN = nearest_neighbors(test_set[8], 3)
 
 # print(Counter(NN).most_common(1))
 
 
-def weighted_KNN_classification(test_set, element,k):
+def unweighted_KNN_classification(test_set, element,k):
     total_distances = []
     index = 0
+    
     for x in test_set:
         total_distances.append([euclidian_distance([x[i] for i in range(len(x)-1)],element),  qualities[index]])
         index += 1
 
     total_distances.sort()
-    #distances_to_k = [total_distances[i] for i in range(k+1)[1:k+1]]
+
     distances_to_k = [total_distances[i] for i in range(k)]
 
     #we do the implementation below because for any k value, we need to keep track of the classes that appear to be our NN
@@ -112,53 +113,92 @@ def weighted_KNN_classification(test_set, element,k):
         #if distance[-1] == (float)above_dict[i]:
         above_dict[int(distance[-1])].append(distance[0])
 
-    #above_dict_trimmed = filter(None, above_dict) #empty list
-
-    #line of code below removes all empty lists from above_dict (syntax may be a little wrong. Maybe find another way to do it)
-
-    #above_dict(filter(lambda x: x, lst))
-    
-
-    #this is now the new above_dict-> {5:[0.054, 0.054],8:{0.057}}
-
-    #START of sum of unweighted distances
-
-    new_list = {} #which will contain class and total sum of distance
+    new_list = {}
 
     for i in above_dict: 
-        s = 0
 
-        for element in above_dict[i]:
-    
-            s += element
+        new_list[(i)] = len(above_dict[i])
 
-        new_list[(i)] = s
 
     return max(new_list, key=new_list.get)
-
-    #END of sum of unweighted distances
-
-    #new_list is now {5:[0.108],8:[0.057]}
-    #how to return the class that has the max?? 
-    # maximum = 0
-    # for i in new_list:
-    #     for distance in new_list[i]:
-    #         if distance > max
-    #         max = distance
-
-    # return new_list[i][max] #idk if the syntax is correct but it will retrieve the class with the updated max
-
-   
-
-    #return -1
 
 
 #first half is test
 #second half we use classify
 
-list_of_ks = [1, 5, 10, 15, 30, 45, 55, 65, 75, 85, 95, 155, 165, 175]
+#list_of_ks = [1, 5, 10, 15, 30, 45, 55, 65, 75, 85, 95, 155, 165, 175]
 
-# list_of_ks = [799]
+list_of_ks = [55]
+
+mid = len(normalized_set)//2
+
+set_of_predictions = []
+
+for k in list_of_ks:
+
+    predictions = []
+
+    for x in range(mid):
+
+        predictions.append(unweighted_KNN_classification(normalized_set[mid:], normalized_set[x], k))
+
+    set_of_predictions.append(predictions)
+
+
+# print(predictions)
+# print(qualities[:mid])
+
+total = 0
+
+for x in set_of_predictions:
+    # print(x)
+    total += correlate_freqs(x, qualities[:mid])
+
+total = total / len(set_of_predictions)
+
+print( "Average error rate w/o weights: " + str(total) )
+
+
+def weighted_KNN_classification(test_set, element,k):
+    total_distances = []
+    
+    index = 0
+    
+    for x in test_set:
+        total_distances.append([euclidian_distance([x[i] for i in range(len(x)-1)],element),  qualities[index]])
+        index += 1
+
+    total_distances.sort()
+
+
+    distances_to_k = [total_distances[i] for i in range(k)]
+    distances = [total_distances[i][0] for i in range(len(distances_to_k))]
+
+    maximum = max(distances)
+    minimum = min(distances)
+
+    #we do the implementation below because for any k value, we need to keep track of the classes that appear to be our NN
+    above_dict = {0:[],1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[],10:[]}
+
+    for distance in distances_to_k:
+        #if distance[-1] == (float)above_dict[i]:
+        above_dict[int(distance[-1])].append(distance[0])
+
+    new_list = [0,0,0,0,0,0,0,0,0,0,0]
+
+    for i in above_dict: 
+        # print(i)
+        for d in above_dict[i]:
+            # print(d)
+            if maximum != minimum:
+                new_list[i] += (maximum - d) / (maximum - minimum)
+            else:
+                new_list[i] += 1
+
+    return new_list.index(max(new_list))
+    
+
+list_of_ks = [55]
 
 mid = len(normalized_set)//2
 
@@ -181,12 +221,48 @@ for k in list_of_ks:
 total = 0
 
 for x in set_of_predictions:
-
+    # print(x)
     total += correlate_freqs(x, qualities[:mid])
 
 total = total / len(set_of_predictions)
 
-print( "Average error rate w/o weights: " + str(1 - (total / 799)) )
+print( "Average error rate w/weights: " + str(total) )
+
+
+
+#     distances_to_k = [total_distances[i] for i in range(k)]
+
+
+#     maximum = max(distances_to_k)
+#     minimum = min(distances_to_k)
+
+#     #we do the implementation below because for any k value, we need to keep track of the classes that appear to be our NN
+#     above_dict = {0:[],1:[],2:[],3:[],4:[],5:[0.057,0.067],6:[],7:[],8:[0.082],9:[],10:[]}
+# #5:[w] 8:[z]
+
+    
+
+#     for distance in distances_to_k:
+ 
+#         above_dict[int(distance[-1])].append(distance[0])
+
+# #
+#     new_list = {} #which will contain class and total sum of distance
+
+#     for i in above_dict: 
+#         s = 0
+
+#         for element in above_dict[i]:
+    
+#             s += element
+
+#         new_list[(i)] = s
+
+#     return max(new_list, key=new_list.get)
+
+
+
+
 
 
 #potentially choosing the optimal K if we have time
